aad995c62fc32cd9e2e37eaa4ad8d671fde93003
sideyewa <85548205+sideyewa@users.noreply.github.com>
2025-07-17 09:01:15 +0400

    init

---
+++ src/main/java/sid/joshi/sid/HealthCheckController.java
@@ -0,0 +1,16 @@
+package sid.joshi.sid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HealthCheckController {
+	@GetMapping("/health")
+	public ResponseEntity<String> healthCheck() {
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+}
